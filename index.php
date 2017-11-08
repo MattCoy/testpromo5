f339daa8305413bcbe46b5507dbc822dae0730bd
MattCoy <contact@le-web-developpeur.com>
2017-11-08 15:48:46 +0100

    html et bootstrap

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<img src="https://www.smashingmagazine.com/wp-content/uploads/2015/06/07-functions-opt-small.jpg" class="img img-responsive">
+			</div>
+			<div class="col-md-6">
+				<img src="https://static.independent.co.uk/s3fs-public/styles/article_small/public/thumbnails/image/2017/09/12/11/naturo-monkey-selfie.jpg" class="img img-responsive">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
